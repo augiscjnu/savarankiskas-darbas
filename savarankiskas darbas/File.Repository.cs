6356b7d28e4e986796e037157e7b468fc31c249a
augiscjnu <gustas1158@gmail.com>
2024-10-30 17:01:28 +0200

    Add project files.

---
+++ savarankiskas darbas/File.Repository.cs
@@ -0,0 +1,32 @@
+﻿using Knygos.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Knygos.Core.Repositories
+{
+    public class KnyguRepository
+    {
+        private List<Knyga> knygos;
+
+        public KnyguRepository(string fileLocation)
+        {
+            knygos = File.ReadLines(fileLocation)
+                .Select(eilute =>
+                {
+                    var duomenys = eilute.Split(',');
+                    return new Knyga(int.Parse(duomenys[0]), duomenys[1], duomenys[2], int.Parse(duomenys[3]), duomenys[4]);
+                }).ToList();
+        }
+
+        public List<Knyga> RastiKnygasPagalAutoriu(string autorius) =>
+            knygos.Where(k => k.Autorius.Contains(autorius, StringComparison.OrdinalIgnoreCase)).ToList();
+
+        public List<Knyga> RastiKnygasPagalMetus(int metai) =>
+            knygos.Where(k => k.IsleidimoMetai >= metai).ToList();
+
+        public List<Knyga> RastiKlasikosKnygas() =>
+            knygos.Where(k => k.Zanras.Equals("Classic", StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+}
---
+++ savarankiskas darbas/Program.cs
@@ -0,0 +1,81 @@
+﻿using Knygos.Core.Models;
+using Knygos.Core.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Knygos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var repository = new KnyguRepository("knygos.txt");
+
+            while (true)
+            {
+                Console.WriteLine("Pasirinkite veiksmą:\n1. Pagal autorių\n2. Pagal metus\n3. Klasikos žanras\n4. Baigti");
+                string pasirinkimas = Console.ReadLine();
+
+                switch (pasirinkimas)
+                {
+                    case "1":
+                        RodytiKnygasPagalAutoriu(repository);
+                        break;
+                    case "2":
+                        RodytiKnygasPagalMetus(repository);
+                        break;
+                    case "3":
+                        RodytiKlasikosKnygas(repository);
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Neteisingas pasirinkimas.");
+                        break;
+                }
+            }
+        }
+
+        static void RodytiKnygasPagalAutoriu(KnyguRepository repository)
+        {
+            Console.Write("Autorius: ");
+            var rezultatai = repository.RastiKnygasPagalAutoriu(Console.ReadLine());
+            RodytiKnygas(rezultatai);
+        }
+
+        static void RodytiKnygasPagalMetus(KnyguRepository repository)
+        {
+            Console.Write("Metai: ");
+            if (int.TryParse(Console.ReadLine(), out int metai))
+            {
+                var rezultatai = repository.RastiKnygasPagalMetus(metai);
+                RodytiKnygas(rezultatai);
+            }
+            else
+            {
+                Console.WriteLine("Neteisingi metai.");
+            }
+        }
+
+        static void RodytiKlasikosKnygas(KnyguRepository repository)
+        {
+            var rezultatai = repository.RastiKlasikosKnygas();
+            RodytiKnygas(rezultatai);
+        }
+
+        static void RodytiKnygas(List<Knyga> knygos)
+        {
+            if (knygos.Count > 0)
+            {
+                foreach (var knyga in knygos)
+                {
+                    Console.WriteLine($"{knyga.KnygosID}: {knyga.Pavadinimas}, {knyga.Autorius}, {knyga.IsleidimoMetai}, {knyga.Zanras}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Knygų nerasta.");
+            }
+        }
+    }
+}
